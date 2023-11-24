a1fe17a7339cfab06c08ec5e841428003cfac2a9
gcp91020 <gcp91020@gmail.com>
2023-11-24 17:51:09 +0800

    updated

---
+++ data/2023/11/hourly_2023-11-19.js
@@ -0,0 +1,85 @@
+var hourly_created="2023-11-19 00:00:00"
+var hourly_updated="2023-11-19 23:59"
+var disk_utilization=""
+var serverUptime="9873.15"
+var freeMem="",availMem="",totMem=""
+
+// Hour: 00
+hourlyData4({ "id":"00:10:db:ff:10:02-10.249.48.1", "hour":"00", "traffic":"656,684" })
+hourlyData4({ "id":"20:76:93:60:02:15-10.249.48.214", "hour":"00", "traffic":"318789,56066" })
+hourlyData4({ "id":"20:76:93:60:02:20-192.168.118.254", "hour":"00", "traffic":"12992,4872" })
+hourlyData4({ "id":"20:76:93:60:02:13-192.168.128.254", "hour":"00", "traffic":"4314,19204" })
+Totals({ "hour":"00", "uptime":"13473.06", "interval":"341914,80826","interfaces":'[ {"n":"lan3", "t":"0,0"}, {"n":"wan", "t":"64716,2265716"}, {"n":"br-lan", "t":"1260,0"}]',"memory":'341780,375684,443976',"disk_utilization":'3%' })
+
+// Hour: 01
+hourlyData4({ "id":"00:00:00:11:22:33-192.168.125.0/24", "hour":"01", "traffic":"264,180" })
+hourlyData4({ "id":"00:10:db:ff:10:02-10.249.48.1", "hour":"01", "traffic":"656,684" })
+hourlyData4({ "id":"20:76:93:60:02:15-10.249.48.214", "hour":"01", "traffic":"380354,45409" })
+hourlyData4({ "id":"20:76:93:60:02:20-192.168.118.254", "hour":"01", "traffic":"12992,4872" })
+Totals({ "hour":"01", "uptime":"17073.10", "interval":"397667,51145","interfaces":'[ {"n":"lan3", "t":"0,0"}, {"n":"wan", "t":"55095,2251513"}, {"n":"br-lan", "t":"1764,0"}]',"memory":'338656,372572,443976',"disk_utilization":'3%' })
+
+// Hour: 02
+hourlyData4({ "id":"00:10:db:ff:10:02-10.249.48.1", "hour":"02", "traffic":"656,684" })
+hourlyData4({ "id":"20:76:93:60:02:15-10.249.48.214", "hour":"02", "traffic":"1306069,46033" })
+hourlyData4({ "id":"20:76:93:60:02:20-192.168.118.254", "hour":"02", "traffic":"12992,4872" })
+Totals({ "hour":"02", "uptime":"20673.08", "interval":"1323920,51589","interfaces":'[ {"n":"lan3", "t":"0,0"}, {"n":"wan", "t":"56769,2737125"}, {"n":"br-lan", "t":"1260,0"}]',"memory":'336460,370964,443976',"disk_utilization":'3%' })
+
+// Hour: 03
+hourlyData4({ "id":"00:10:db:ff:10:02-10.249.48.1", "hour":"03", "traffic":"656,684" })
+hourlyData4({ "id":"20:76:93:60:02:15-10.249.48.214", "hour":"03", "traffic":"285110,32663" })
+hourlyData4({ "id":"20:76:93:60:02:20-192.168.118.254", "hour":"03", "traffic":"12992,4872" })
+Totals({ "hour":"03", "uptime":"24273.12", "interval":"302237,38219","interfaces":'[ {"n":"lan3", "t":"0,0"}, {"n":"wan", "t":"40361,2174989"}, {"n":"br-lan", "t":"1260,0"}]',"memory":'337800,372320,443976',"disk_utilization":'3%' })
+
+// Hour: 04
+hourlyData4({ "id":"00:10:db:ff:10:02-10.249.48.1", "hour":"04", "traffic":"656,684" })
+hourlyData4({ "id":"20:76:93:60:02:15-10.249.48.214", "hour":"04", "traffic":"325080,36561" })
+hourlyData4({ "id":"20:76:93:60:02:20-192.168.118.254", "hour":"04", "traffic":"12992,4872" })
+Totals({ "hour":"04", "uptime":"27873.18", "interval":"342207,42117","interfaces":'[ {"n":"lan3", "t":"0,0"}, {"n":"wan", "t":"44819,2176567"}, {"n":"br-lan", "t":"1512,0"}]',"memory":'335260,369792,443976',"disk_utilization":'3%' })
+
+// Hour: 05
+hourlyData4({ "id":"00:10:db:ff:10:02-10.249.48.1", "hour":"05", "traffic":"656,684" })
+hourlyData4({ "id":"20:76:93:60:02:15-10.249.48.214", "hour":"05", "traffic":"264664,32565" })
+hourlyData4({ "id":"20:76:93:60:02:20-192.168.118.254", "hour":"05", "traffic":"12992,4956" })
+Totals({ "hour":"05", "uptime":"31473.23", "interval":"281791,38205","interfaces":'[ {"n":"lan3", "t":"0,0"}, {"n":"wan", "t":"40319,2148886"}, {"n":"br-lan", "t":"1260,0"}]',"memory":'334232,368776,443976',"disk_utilization":'3%' })
+
+// Hour: 06
+hourlyData4({ "id":"20:76:93:60:02:15-10.249.48.214", "hour":"06", "traffic":"48806,5904" })
+hourlyData4({ "id":"20:76:93:60:02:20-192.168.118.254", "hour":"06", "traffic":"2240,756" })
+Totals({ "hour":"06", "uptime":"32075.14", "interval":"54764,6660","interfaces":'[ {"n":"lan3", "t":"0,0"}, {"n":"wan", "t":"7080,363684"}, {"n":"br-lan", "t":"252,0"}]',"memory":'334700,369244,443976',"disk_utilization":'3%' })
+//Server Rebooted
+
+// Hour: 19
+hourlyData4({ "id":"66:b2:42:39:d4:2d-192.168.128.222", "hour":"19", "traffic":"379447,114737" })
+hourlyData4({ "id":"00:10:db:ff:10:02-10.249.48.1", "hour":"19", "traffic":"328,342" })
+hourlyData4({ "id":"20:76:93:60:02:15-10.249.48.218", "hour":"19", "traffic":"39169,12756" })
+hourlyData4({ "id":"20:76:93:60:02:20-192.168.118.254", "hour":"19", "traffic":"20568,8204" })
+Totals({ "hour":"19", "uptime":"2672.02", "interval":"445140,136039","interfaces":'[ {"n":"lan3", "t":"0,0"}, {"n":"wan", "t":"1496171,5415398"}, {"n":"br-lan", "t":"452218,165989"}]',"memory":'354148,379464,443976',"disk_utilization":'2%' })
+
+// Hour: 20
+hourlyData4({ "id":"66:b2:42:39:d4:2d-192.168.128.222", "hour":"20", "traffic":"1075974,556377" })
+hourlyData4({ "id":"00:10:db:ff:10:02-10.249.48.1", "hour":"20", "traffic":"656,684" })
+hourlyData4({ "id":"20:76:93:60:02:15-10.249.48.218", "hour":"20", "traffic":"57830,13113" })
+hourlyData4({ "id":"20:76:93:60:02:20-192.168.118.254", "hour":"20", "traffic":"12992,4872" })
+Totals({ "hour":"20", "uptime":"6272.07", "interval":"1152578,575046","interfaces":'[ {"n":"lan3", "t":"0,0"}, {"n":"wan", "t":"3858249,12605445"}, {"n":"br-lan", "t":"1246812,669687"}]',"memory":'353832,379164,443976',"disk_utilization":'2%' })
+
+// Hour: 21
+//Server Rebooted
+hourlyData4({ "id":"66:b2:42:39:d4:2d-192.168.128.222", "hour":"21", "traffic":"711170,505304" })
+hourlyData4({ "id":"00:10:db:ff:10:02-10.249.48.1", "hour":"21", "traffic":"328,342" })
+hourlyData4({ "id":"20:76:93:60:02:15-10.249.48.219", "hour":"21", "traffic":"45429,12590" })
+hourlyData4({ "id":"20:76:93:60:02:20-192.168.118.254", "hour":"21", "traffic":"20568,8204" })
+Totals({ "hour":"21", "uptime":"2673.04", "interval":"781447,526440","interfaces":'[ {"n":"lan3", "t":"0,0"}, {"n":"wan", "t":"3021642,9610623"}, {"n":"br-lan", "t":"885302,603025"}]',"memory":'348704,374080,443976',"disk_utilization":'2%' })
+
+// Hour: 22
+hourlyData4({ "id":"66:b2:42:39:d4:2d-192.168.128.222", "hour":"22", "traffic":"1237819,695249" })
+hourlyData4({ "id":"00:10:db:ff:10:02-10.249.48.1", "hour":"22", "traffic":"656,684" })
+hourlyData4({ "id":"20:76:93:60:02:15-10.249.48.219", "hour":"22", "traffic":"72743,12967" })
+hourlyData4({ "id":"20:76:93:60:02:20-192.168.118.254", "hour":"22", "traffic":"12992,4872" })
+Totals({ "hour":"22", "uptime":"6273.10", "interval":"1327928,713772","interfaces":'[ {"n":"lan3", "t":"0,0"}, {"n":"wan", "t":"4353590,20441981"}, {"n":"br-lan", "t":"1469118,846806"}]',"memory":'351328,376720,443976',"disk_utilization":'2%' })
+
+// Hour: 23
+hourlyData4({ "id":"66:b2:42:39:d4:2d-192.168.128.222", "hour":"23", "traffic":"566685,302333" })
+hourlyData4({ "id":"00:10:db:ff:10:02-10.249.48.1", "hour":"23", "traffic":"656,342" })
+hourlyData4({ "id":"20:76:93:60:02:15-10.249.48.219", "hour":"23", "traffic":"180150,28293" })
+hourlyData4({ "id":"20:76:93:60:02:20-192.168.118.254", "hour":"23", "traffic":"12992,4872" })
+Totals({ "hour":"23", "uptime":"9873.15", "interval":"764435,335840","interfaces":'[ {"n":"lan3", "t":"0,0"}, {"n":"wan", "t":"1958757,8520517"}, {"n":"br-lan", "t":"654285,351284"}]',"memory":'342960,375672,443976',"disk_utilization":'2%' })
---
+++ data/2023/11/hourly_2023-11-23.js
@@ -0,0 +1,168 @@
+var hourly_created="2023-11-23 00:00:00"
+var hourly_updated="2023-11-23 23:59"
+var disk_utilization=""
+var serverUptime="6273.15"
+var freeMem="",availMem="",totMem=""
+
+// Hour: 00
+hourlyData4({ "id":"00:10:db:ff:10:02-10.249.48.1", "hour":"00", "traffic":"656,684" })
+hourlyData4({ "id":"20:76:93:60:02:15-10.249.48.238", "hour":"00", "traffic":"25808,11600" })
+Totals({ "hour":"00", "uptime":"9872.53", "interval":"48603,12284","interfaces":'[ {"n":"wan", "t":"16708,2441816"}, {"n":"br-lan", "t":"0,2734"}]',"memory":'358400,381968,443976',"disk_utilization":'3%' })
+
+// Hour: 01
+hourlyData4({ "id":"00:10:db:ff:10:02-10.249.48.1", "hour":"01", "traffic":"656,684" })
+hourlyData4({ "id":"20:76:93:60:02:15-10.249.48.238", "hour":"01", "traffic":"26704,12436" })
+Totals({ "hour":"01", "uptime":"13472.57", "interval":"50109,13120","interfaces":'[ {"n":"wan", "t":"17740,2441596"}, {"n":"br-lan", "t":"0,0"}]',"memory":'356924,380500,443976',"disk_utilization":'3%' })
+
+// Hour: 02
+hourlyData4({ "id":"00:10:db:ff:10:02-10.249.48.1", "hour":"02", "traffic":"656,684" })
+hourlyData4({ "id":"20:76:93:60:02:15-10.249.48.238", "hour":"02", "traffic":"25192,10732" })
+Totals({ "hour":"02", "uptime":"17072.62", "interval":"47748,11416","interfaces":'[ {"n":"wan", "t":"15672,2500761"}, {"n":"br-lan", "t":"0,0"}]',"memory":'356868,380456,443976',"disk_utilization":'3%' })
+
+// Hour: 03
+hourlyData4({ "id":"00:10:db:ff:10:02-10.249.48.1", "hour":"03", "traffic":"656,684" })
+hourlyData4({ "id":"20:76:93:60:02:15-10.249.48.238", "hour":"03", "traffic":"24804,10460" })
+Totals({ "hour":"03", "uptime":"20672.67", "interval":"48726,11144","interfaces":'[ {"n":"wan", "t":"15442,2692730"}, {"n":"br-lan", "t":"0,0"}]',"memory":'356944,380540,443976',"disk_utilization":'3%' })
+
+// Hour: 04
+hourlyData4({ "id":"00:10:db:ff:10:02-10.249.48.1", "hour":"04", "traffic":"656,684" })
+hourlyData4({ "id":"20:76:93:60:02:15-10.249.48.238", "hour":"04", "traffic":"24692,10352" })
+Totals({ "hour":"04", "uptime":"24272.72", "interval":"48032,11036","interfaces":'[ {"n":"wan", "t":"15138,2644079"}, {"n":"br-lan", "t":"0,0"}]',"memory":'356708,380316,443976',"disk_utilization":'3%' })
+
+// Hour: 05
+hourlyData4({ "id":"00:10:db:ff:10:02-10.249.48.1", "hour":"05", "traffic":"656,684" })
+hourlyData4({ "id":"20:76:93:60:02:15-10.249.48.238", "hour":"05", "traffic":"25208,10692" })
+Totals({ "hour":"05", "uptime":"27872.77", "interval":"48896,11376","interfaces":'[ {"n":"wan", "t":"15702,3313595"}, {"n":"br-lan", "t":"0,0"}]',"memory":'357592,381212,443976',"disk_utilization":'3%' })
+
+// Hour: 06
+//Server Rebooted
+hourlyData4({ "id":"00:10:db:ff:10:02-10.249.48.1", "hour":"06", "traffic":"328,342" })
+hourlyData4({ "id":"20:76:93:60:02:15-10.249.48.242", "hour":"06", "traffic":"20932,13656" })
+Totals({ "hour":"06", "uptime":"2672.82", "interval":"43288,13998","interfaces":'[ {"n":"wan", "t":"18744,1907617"}, {"n":"br-lan", "t":"0,0"}]',"memory":'361236,383596,443976',"disk_utilization":'2%' })
+
+// Hour: 07
+hourlyData4({ "id":"00:00:00:11:22:33-192.168.125.0/24", "hour":"07", "traffic":"264,180" })
+hourlyData4({ "id":"00:10:db:ff:10:02-10.249.48.1", "hour":"07", "traffic":"656,684" })
+hourlyData4({ "id":"20:76:93:60:02:15-10.249.48.242", "hour":"07", "traffic":"26256,12152" })
+Totals({ "hour":"07", "uptime":"6272.95", "interval":"50094,13016","interfaces":'[ {"n":"wan", "t":"17400,2580485"}, {"n":"br-lan", "t":"252,0"}]',"memory":'359640,382016,443976',"disk_utilization":'2%' })
+
+// Hour: 08
+hourlyData4({ "id":"00:10:db:ff:10:02-10.249.48.1", "hour":"08", "traffic":"656,684" })
+hourlyData4({ "id":"20:76:93:60:02:15-10.249.48.242", "hour":"08", "traffic":"26974,12078" })
+Totals({ "hour":"08", "uptime":"9873.16", "interval":"65665,12762","interfaces":'[ {"n":"wan", "t":"49376,2791684"}, {"n":"br-lan", "t":"4402,16477"}]',"memory":'358624,381016,443976',"disk_utilization":'2%' })
+
+// Hour: 09
+hourlyData4({ "id":"66:b2:42:39:d4:2d-192.168.128.222", "hour":"09", "traffic":"1468,16240" })
+hourlyData4({ "id":"00:10:db:ff:10:02-10.249.48.1", "hour":"09", "traffic":"656,684" })
+hourlyData4({ "id":"20:76:93:60:02:15-10.249.48.242", "hour":"09", "traffic":"26423,11773" })
+Totals({ "hour":"09", "uptime":"13473.05", "interval":"60841,28697","interfaces":'[ {"n":"wan", "t":"52181,2751965"}, {"n":"br-lan", "t":"7860,22010"}]',"memory":'357920,380336,443976',"disk_utilization":'2%' })
+
+// Hour: 10
+hourlyData4({ "id":"66:b2:42:39:d4:2d-192.168.128.222", "hour":"10", "traffic":"1256,16212" })
+hourlyData4({ "id":"00:10:db:ff:10:02-10.249.48.1", "hour":"10", "traffic":"656,684" })
+hourlyData4({ "id":"20:76:93:60:02:15-10.249.48.242", "hour":"10", "traffic":"25607,11239" })
+Totals({ "hour":"10", "uptime":"17073.10", "interval":"54971,28135","interfaces":'[ {"n":"wan", "t":"51387,3355351"}, {"n":"br-lan", "t":"6653,21211"}]',"memory":'357984,380404,443976',"disk_utilization":'2%' })
+
+// Hour: 11
+hourlyData4({ "id":"66:b2:42:39:d4:2d-192.168.128.222", "hour":"11", "traffic":"1256,16148" })
+hourlyData4({ "id":"00:10:db:ff:10:02-10.249.48.1", "hour":"11", "traffic":"656,684" })
+hourlyData4({ "id":"20:76:93:60:02:15-10.249.48.242", "hour":"11", "traffic":"24879,10451" })
+Totals({ "hour":"11", "uptime":"20672.14", "interval":"53086,27283","interfaces":'[ {"n":"wan", "t":"50431,3776373"}, {"n":"br-lan", "t":"6363,21009"}]',"memory":'357712,380148,443976',"disk_utilization":'2%' })
+
+// Hour: 12
+hourlyData4({ "id":"66:b2:42:39:d4:2d-192.168.128.222", "hour":"12", "traffic":"1488,16776" })
+hourlyData4({ "id":"00:10:db:ff:10:02-10.249.48.1", "hour":"12", "traffic":"656,684" })
+hourlyData4({ "id":"20:76:93:60:02:15-10.249.48.242", "hour":"12", "traffic":"25237,10437" })
+Totals({ "hour":"12", "uptime":"24272.20", "interval":"59371,27897","interfaces":'[ {"n":"wan", "t":"55406,3893070"}, {"n":"br-lan", "t":"8189,22819"}]',"memory":'357792,380236,443976',"disk_utilization":'2%' })
+
+// Hour: 13
+hourlyData4({ "id":"66:b2:42:39:d4:2d-192.168.128.222", "hour":"13", "traffic":"1316,16088" })
+hourlyData4({ "id":"00:10:db:ff:10:02-10.249.48.1", "hour":"13", "traffic":"656,684" })
+hourlyData4({ "id":"20:76:93:60:02:15-10.249.48.242", "hour":"13", "traffic":"55338,22215" })
+Totals({ "hour":"13", "uptime":"27872.82", "interval":"205827,38987","interfaces":'[ {"n":"wan", "t":"818322,6048431"}, {"n":"br-lan", "t":"85617,191163"}]',"memory":'357292,379756,443976',"disk_utilization":'2%' })
+
+// Hour: 14
+hourlyData4({ "id":"66:bb:89:25:0c:21-192.168.128.206", "hour":"14", "traffic":"43706,41727" })
+hourlyData4({ "id":"3c:06:30:31:bf:66-192.168.128.162", "hour":"14", "traffic":"775693,3517177" })
+hourlyData4({ "id":"66:b2:42:39:d4:2d-192.168.128.222", "hour":"14", "traffic":"1451,16508" })
+hourlyData4({ "id":"00:10:db:ff:10:02-10.249.48.1", "hour":"14", "traffic":"656,684" })
+hourlyData4({ "id":"20:76:93:60:02:15-10.249.48.242", "hour":"14", "traffic":"144976,62967" })
+Totals({ "hour":"14", "uptime":"31472.45", "interval":"991129,3639063","interfaces":'[ {"n":"wan", "t":"27652590,168231417"}, {"n":"br-lan", "t":"1076642,4163928"}]',"memory":'360824,383304,443976',"disk_utilization":'2%' })
+
+// Hour: 15
+hourlyData4({ "id":"66:bb:89:25:0c:21-192.168.128.206", "hour":"15", "traffic":"26246,25156" })
+hourlyData4({ "id":"3c:06:30:31:bf:66-192.168.128.162", "hour":"15", "traffic":"530298,2421520" })
+hourlyData4({ "id":"66:b2:42:39:d4:2d-192.168.128.222", "hour":"15", "traffic":"2108,7388" })
+hourlyData4({ "id":"04:42:1a:19:52:a0-192.168.50.1", "hour":"15", "traffic":"7279,14283" })
+hourlyData4({ "id":"00:10:db:ff:10:02-10.249.48.1", "hour":"15", "traffic":"1724,1368" })
+hourlyData4({ "id":"20:76:93:60:02:15-10.249.48.242", "hour":"15", "traffic":"112572,51958" })
+Totals({ "hour":"15", "uptime":"35072.63", "interval":"819241,2521673","interfaces":'[ {"n":"wan", "t":"26106382,42548493"}, {"n":"br-lan", "t":"10453771,4478549"}]',"memory":'354416,380060,443976',"disk_utilization":'2%' })
+
+// Hour: 16
+hourlyData4({ "id":"66:bb:89:25:0c:21-192.168.128.206", "hour":"16", "traffic":"26085,24566" })
+hourlyData4({ "id":"3c:06:30:31:bf:66-192.168.128.162", "hour":"16", "traffic":"633057,1930867" })
+hourlyData4({ "id":"66:b2:42:39:d4:2d-192.168.128.222", "hour":"16", "traffic":"1256,2168" })
+hourlyData4({ "id":"04:42:1a:19:52:a0-192.168.50.1", "hour":"16", "traffic":"6343,10369" })
+hourlyData4({ "id":"00:10:db:ff:10:02-10.249.48.1", "hour":"16", "traffic":"656,684" })
+Totals({ "hour":"16", "uptime":"38672.61", "interval":"737755,1968654","interfaces":'[ {"n":"wan", "t":"54314335,502112207"}, {"n":"br-lan", "t":"27695577,5625899"}]',"memory":'352636,378572,443976',"disk_utilization":'2%' })
+
+// Hour: 17
+hourlyData4({ "id":"66:bb:89:25:0c:21-192.168.128.206", "hour":"17", "traffic":"30618,139722" })
+hourlyData4({ "id":"3c:06:30:31:bf:66-192.168.128.162", "hour":"17", "traffic":"13477379,3906920" })
+hourlyData4({ "id":"66:b2:42:39:d4:2d-192.168.128.222", "hour":"17", "traffic":"1216,4876" })
+hourlyData4({ "id":"00:00:00:11:22:33-192.168.125.0/24", "hour":"17", "traffic":"65268,0" })
+hourlyData4({ "id":"00:10:db:ff:10:02-10.249.48.1", "hour":"17", "traffic":"2792,1710" })
+Totals({ "hour":"17", "uptime":"42272.60", "interval":"18347959,4053228","interfaces":'[ {"n":"wan", "t":"9746332,72823229"}, {"n":"br-lan", "t":"28506570,7399814"}]',"memory":'354048,380052,443976',"disk_utilization":'2%' })
+
+// Hour: 18
+hourlyData4({ "id":"be:0f:73:3f:ab:40-192.168.128.218", "hour":"18", "traffic":"0,96" })
+hourlyData4({ "id":"66:bb:89:25:0c:21-192.168.128.206", "hour":"18", "traffic":"106714,111898" })
+hourlyData4({ "id":"3c:06:30:31:bf:66-192.168.128.162", "hour":"18", "traffic":"5498943,2465279" })
+hourlyData4({ "id":"66:b2:42:39:d4:2d-192.168.128.222", "hour":"18", "traffic":"1256,1808" })
+hourlyData4({ "id":"00:00:00:11:22:33-192.168.125.0/24", "hour":"18", "traffic":"7587,168" })
+hourlyData4({ "id":"be:0f:73:3f:ab:40-192.168.118.218", "hour":"18", "traffic":"5460,0" })
+hourlyData4({ "id":"00:10:db:ff:10:02-10.249.48.1", "hour":"18", "traffic":"656,684" })
+Totals({ "hour":"18", "uptime":"45872.81", "interval":"8800859,2579933","interfaces":'[ {"n":"wan", "t":"6370296,29426326"}, {"n":"br-lan", "t":"9921465,4597828"}]',"memory":'354584,380624,443976',"disk_utilization":'2%' })
+
+// Hour: 19
+hourlyData4({ "id":"66:bb:89:25:0c:21-192.168.128.206", "hour":"19", "traffic":"9677,6351" })
+hourlyData4({ "id":"3c:06:30:31:bf:66-192.168.128.162", "hour":"19", "traffic":"412103,480474" })
+hourlyData4({ "id":"66:b2:42:39:d4:2d-192.168.128.222", "hour":"19", "traffic":"1256,1808" })
+hourlyData4({ "id":"00:00:00:11:22:33-192.168.125.0/24", "hour":"19", "traffic":"251263,0" })
+hourlyData4({ "id":"00:10:db:ff:10:02-10.249.48.1", "hour":"19", "traffic":"656,684" })
+Totals({ "hour":"19", "uptime":"49472.69", "interval":"856708,489317","interfaces":'[ {"n":"wan", "t":"638262,3190860"}, {"n":"br-lan", "t":"921065,885960"}]',"memory":'358028,384088,443976',"disk_utilization":'2%' })
+
+// Hour: 20
+hourlyData4({ "id":"3c:06:30:31:bf:66-192.168.128.162", "hour":"20", "traffic":"100800,0" })
+hourlyData4({ "id":"66:b2:42:39:d4:2d-192.168.128.222", "hour":"20", "traffic":"14934,26724" })
+hourlyData4({ "id":"00:00:00:11:22:33-192.168.125.0/24", "hour":"20", "traffic":"148016,0" })
+hourlyData4({ "id":"00:10:db:ff:10:02-10.249.48.1", "hour":"20", "traffic":"656,684" })
+hourlyData4({ "id":"20:76:93:60:02:13-192.168.128.254", "hour":"20", "traffic":"681443,4673308" })
+Totals({ "hour":"20", "uptime":"53072.74", "interval":"3867253,4700716","interfaces":'[ {"n":"wan", "t":"1578138,3274807"}, {"n":"br-lan", "t":"135523,71023"}]',"memory":'357256,383324,443976',"disk_utilization":'2%' })
+
+// Hour: 21
+hourlyData4({ "id":"66:b2:42:39:d4:2d-192.168.128.222", "hour":"21", "traffic":"16092,13804" })
+hourlyData4({ "id":"be:0f:73:3f:ab:40-192.168.118.218", "hour":"21", "traffic":"30020,18050" })
+hourlyData4({ "id":"00:10:db:ff:10:02-10.249.48.1", "hour":"21", "traffic":"656,684" })
+hourlyData4({ "id":"20:76:93:60:02:13-192.168.128.254", "hour":"21", "traffic":"1555364,10392959" })
+Totals({ "hour":"21", "uptime":"56672.71", "interval":"6660435,10425497","interfaces":'[ {"n":"wan", "t":"11707355,5118155"}, {"n":"br-lan", "t":"15255,19975"}]',"memory":'352460,378548,443976',"disk_utilization":'2%' })
+
+// Hour: 22
+//Server Rebooted
+hourlyData4({ "id":"66:b2:42:39:d4:2d-192.168.128.222", "hour":"22", "traffic":"49440,10648" })
+hourlyData4({ "id":"00:00:00:11:22:33-192.168.125.0/24", "hour":"22", "traffic":"1008,168" })
+hourlyData4({ "id":"be:0f:73:3f:ab:40-192.168.118.218", "hour":"22", "traffic":"0,497" })
+hourlyData4({ "id":"00:10:db:ff:10:02-10.249.48.1", "hour":"22", "traffic":"328,342" })
+hourlyData4({ "id":"20:76:93:60:02:15-10.249.48.250", "hour":"22", "traffic":"761700,2426340" })
+hourlyData4({ "id":"20:76:93:60:02:20-192.168.128.217", "hour":"22", "traffic":"57688,65372" })
+hourlyData4({ "id":"20:76:93:60:02:13-192.168.128.254", "hour":"22", "traffic":"482829,2199580" })
+Totals({ "hour":"22", "uptime":"2673.17", "interval":"1376627,4702947","interfaces":'[ {"n":"lan3", "t":"95607,177594"}, {"n":"wan", "t":"2860485,2867014"}, {"n":"br-lan", "t":"63998,19380"}]',"memory":'356144,381428,443976',"disk_utilization":'2%' })
+
+// Hour: 23
+hourlyData4({ "id":"be:0f:73:3f:ab:40-192.168.127.218", "hour":"23", "traffic":"28226,44478" })
+hourlyData4({ "id":"66:b2:42:39:d4:2d-192.168.128.222", "hour":"23", "traffic":"5488,11244" })
+hourlyData4({ "id":"00:10:db:ff:10:02-10.249.48.1", "hour":"23", "traffic":"656,684" })
+hourlyData4({ "id":"20:76:93:60:02:15-10.249.48.250", "hour":"23", "traffic":"2733618,11088599" })
+hourlyData4({ "id":"20:76:93:60:02:20-192.168.128.217", "hour":"23", "traffic":"504,504" })
+hourlyData4({ "id":"20:76:93:60:02:13-192.168.128.254", "hour":"23", "traffic":"1638099,10063966" })
+Totals({ "hour":"23", "uptime":"6273.15", "interval":"4433337,21209475","interfaces":'[ {"n":"lan3", "t":"48864,82893"}, {"n":"wan", "t":"11436633,5264998"}, {"n":"br-lan", "t":"12209,17198"}]',"memory":'355404,381100,443976',"disk_utilization":'2%' })
